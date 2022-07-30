d3bb2b0579873dc0db3e5b82ebf9eb60cc9e8f51
Facundo Cuadrado <hola9811@hotmail.com>
2022-07-30 11:29:03 -0300

    Creating repository

---
+++ src/main/java/net/faxu/lostworld/LostWorld.java
@@ -0,0 +1,15 @@
+package net.faxu.lostworld;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LostWorld implements ModInitializer {
+	public static final String MOD_ID = "lostworld";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/faxu/lostworld/LostWorldClient.java
@@ -0,0 +1,10 @@
+package net.faxu.lostworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LostWorldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
