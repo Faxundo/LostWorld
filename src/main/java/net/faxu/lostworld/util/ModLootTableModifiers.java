bdda59eed773cc3476d69eb54ba61c0282020500
Facundo Cuadrado <hola9811@hotmail.com>
2022-08-04 00:59:17 -0300

    Loot Tables

---
+++ src/main/java/net/faxu/lostworld/util/ModLootTableModifiers.java
@@ -0,0 +1,44 @@
+package net.faxu.lostworld.util;
+
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.faxu.lostworld.item.ModItems;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.MatchToolLootCondition;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.predicate.item.ItemPredicate;
+import net.minecraft.util.Identifier;
+
+public class ModLootTableModifiers {
+    private static final Identifier SUGAR_CANE_ID = new Identifier("minecraft", "blocks/sugar_cane");
+    private static final Identifier SPIDER_CAVE_ID = new Identifier("minecraft", "entities/cave_spider");
+
+    public static void modifyLootTables() {
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (SUGAR_CANE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.6f))
+                        .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create()
+                                        .items(ModItems.FLINT_KNIFE, ModItems.FLINT_KNIFE_POISONED)).build())
+                        .with(ItemEntry.builder(ModItems.FIBER))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            if (SPIDER_CAVE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(0.2f))
+                        .with(ItemEntry.builder(ModItems.POISON_BOTTLE))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0, 1.0f)).build());
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+    }
+
+}
